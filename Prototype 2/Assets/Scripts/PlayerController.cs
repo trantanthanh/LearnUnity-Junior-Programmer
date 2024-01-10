49f1046047090e8ccaca5cd250ddb2f98d88fbd3
Thành Trần Tấn <trantanthanh@gmail.com>
2024-01-10 22:50:55 +0700

    add player controller script

---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float horizontalInput;
+    public float speedMove = 10f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * speedMove * Time.deltaTime * horizontalInput);
+    }
+}
