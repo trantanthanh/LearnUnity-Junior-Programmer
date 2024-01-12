16aee395b02be7c7a371709d79f727a17869176c
Thành Trần Tấn <trantanthanh@gmail.com>
2024-01-13 01:02:06 +0700

    auto spawn randomly

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -7,20 +7,28 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 12;
     private float spawnPosZ = 20;
+
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndexRandom = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndexRandom], spawnPos, animalPrefabs[animalIndexRandom].transform.rotation);
-        }
+        // if (Input.GetKeyDown(KeyCode.S))
+        // {
+        //     SpawnRandomAnimal();
+        // }
+    }
+
+    void SpawnRandomAnimal()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndexRandom = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndexRandom], spawnPos, animalPrefabs[animalIndexRandom].transform.rotation);
     }
 }
