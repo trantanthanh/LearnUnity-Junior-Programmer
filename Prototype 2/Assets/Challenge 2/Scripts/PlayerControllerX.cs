1c64dde6b9077029bb18924c8dcee6837636253b
Thành Trần Tấn <trantanthanh@gmail.com>
2024-01-14 01:57:10 +0700

    use StartCoroutine to spawn random interval, use time counter to prevent spam spawn dog

--- Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float timeIntervalSpawnDog = 1.5f;
 
+    private float timeInterval = 0;
     // Update is called once per frame
     void Update()
     {
+        timeInterval -= Time.deltaTime;
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            if (timeInterval <= 0)
+            {
+                timeInterval = timeIntervalSpawnDog;
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            }
         }
     }
 }
