49418b799ad114235eff9ad7f95a9f0770db6394
Thành Trần Tấn <trantanthanh@gmail.com>
2024-01-25 01:32:11 +0700

    make the player moving by using focal point's forward vector

--- Prototype 4/Assets/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 10f;
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float verticalInput = Input.GetAxis("Vertical");
+        //playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(focalPoint.transform.forward * speed * verticalInput);
+
     }
 }
