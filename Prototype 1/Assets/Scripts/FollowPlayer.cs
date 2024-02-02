b703046e900352756b476193b142d1e081a3c592
Thành Trần Tấn <trantanthanh@gmail.com>
2024-02-03 01:45:08 +0700

    commit , add Object pooling asset

--- Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -5,17 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     [SerializeField] GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -7);
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
+    [SerializeField] Vector3 offset = new Vector3(0, 5, -7);
 
     void LateUpdate()
     {
