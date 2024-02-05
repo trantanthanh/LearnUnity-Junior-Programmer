342bdc15259d806b0f3266d3a9014aa3051cc0b7
Thành Trần Tấn <trantanthanh@gmail.com>
2024-02-05 22:01:12 +0700

    add TextMeshPro to add speedometer text

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -8,9 +9,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float turnSpeed = 10f;
     [SerializeField] float forcePower = 10f;
     [SerializeField] GameObject centerOfMass;
+    [SerializeField] TextMeshProUGUI textSpeedometer;
     private Rigidbody playerRb;
     private float horizontalInput;
     private float forwardInput;
+    private float currentSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,5 +35,12 @@ public class PlayerController : MonoBehaviour
         // turn the vehicle
         // transform.Translate(Vector3.right * turnSpeed * Time.deltaTime * horizontalInput);
         transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+        UpdateSpeedometer();
+    }
+
+    void UpdateSpeedometer()
+    { 
+        currentSpeed = Mathf.Round(playerRb.velocity.magnitude * 3.6f);
+        textSpeedometer.text = "Speed : " + currentSpeed + " km/h";
     }
 }
