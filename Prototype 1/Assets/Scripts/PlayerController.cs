5ff8c78d6db3bb5944ce1fd8dea2f49a8943f758
Thành Trần Tấn <trantanthanh@gmail.com>
2024-02-06 02:42:34 +0700

    check all wheels on the ground or not

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float turnSpeed = 10f;
     [SerializeField] float forcePower = 10f;
     [SerializeField] float wheelRadius = 0.2f;//radius of wheel(meter)
+    [SerializeField] List<WheelCollider> allWheelColliders;
     [SerializeField] GameObject centerOfMass;
     [SerializeField] TextMeshProUGUI textSpeedometer;
     [SerializeField] TextMeshProUGUI textRPM;
@@ -31,22 +32,25 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        // get player input
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
-        // transform.Translate(0, 0, speed * Time.deltaTime);
-        // move the vehicle forward
-        //transform.Translate(Vector3.forward * speed * Time.deltaTime * forwardInput);
-        playerRb.AddRelativeForce(Vector3.forward * forcePower * forwardInput);
+        if (IsOnGround())
+        {
+            // get player input
+            horizontalInput = Input.GetAxis("Horizontal");
+            forwardInput = Input.GetAxis("Vertical");
+            // transform.Translate(0, 0, speed * Time.deltaTime);
+            // move the vehicle forward
+            //transform.Translate(Vector3.forward * speed * Time.deltaTime * forwardInput);
+            playerRb.AddRelativeForce(Vector3.forward * forcePower * forwardInput);
 
-        // turn the vehicle
-        // transform.Translate(Vector3.right * turnSpeed * Time.deltaTime * horizontalInput);
-        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
-        UpdateSpeedometer();
+            // turn the vehicle
+            // transform.Translate(Vector3.right * turnSpeed * Time.deltaTime * horizontalInput);
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+            UpdateSpeedometer();
+        }
     }
 
     void UpdateSpeedometer()
-    { 
+    {
         //Rigidbody.velocity.magnitude is meter/second -> Kilometer/Hour by multiple 3.6
         currentSpeed = Mathf.RoundToInt(playerRb.velocity.magnitude * 3.6f);
         textSpeedometer.text = "Speed : " + currentSpeed + " km/h";
@@ -55,7 +59,19 @@ public class PlayerController : MonoBehaviour
         //circumference = 2 * PI * radius
         //speed KM/H = circumference * RPM * 60 (minutes) / 1000 (m)
         // RPM = (speed KM/H) * 1000/(2*PI*wheelRadius * 6)
-        currentRPM = Mathf.RoundToInt(currentSpeed * 1000 /( 12 * Mathf.PI * wheelRadius));
+        currentRPM = Mathf.RoundToInt(currentSpeed * 1000 / (12 * Mathf.PI * wheelRadius));
         textRPM.text = "RPM : " + currentRPM;
+    }
+
+    bool IsOnGround()
+    {
+        foreach (WheelCollider wheel in allWheelColliders)
+        {
+            if (!wheel.isGrounded)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
