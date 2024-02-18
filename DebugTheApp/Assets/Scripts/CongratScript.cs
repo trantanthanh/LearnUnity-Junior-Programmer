de7a9b8fd87f1d490b7799f5dc4954c372d1b631
Thành Trần Tấn <trantanthanh@gmail.com>
2024-02-19 01:12:41 +0700

    add error package, debug and fixed bugs

---
+++ DebugTheApp/Assets/Scripts/CongratScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CongratScript : MonoBehaviour
+{
+    private TextMesh textMesh;
+    public ParticleSystem SparksParticles;
+
+    private List<string> TextToDisplay = new List<string>();
+
+    private float RotatingSpeed;
+    private float TimeToNextText;
+
+    private int CurrentText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMesh = GetComponent<TextMesh>();
+        TimeToNextText = 0.0f;
+        CurrentText = 0;
+
+        RotatingSpeed = 1.0f;
+        TextToDisplay.Add("Congratulation");
+        TextToDisplay.Add("All Errors Fixed");
+
+        textMesh.text = TextToDisplay[0];
+
+        SparksParticles.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeToNextText += Time.deltaTime;
+
+        if (TimeToNextText > 1.5f)
+        {
+            TimeToNextText = 0.0f;
+
+            CurrentText++;
+            if (CurrentText >= TextToDisplay.Count)
+            {
+                CurrentText = 0;
+            }
+            textMesh.text = TextToDisplay[CurrentText];
+        }
+    }
+}
