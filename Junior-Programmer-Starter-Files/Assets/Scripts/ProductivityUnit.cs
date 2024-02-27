196f2264fcf8f61503407cf3885d5bc179c5ffe1
Thành Trần Tấn <trantanthanh@gmail.com>
2024-02-28 01:31:24 +0700

    check resource pile is in range or not

--- Junior-Programmer-Starter-Files/Assets/Scripts/ProductivityUnit.cs
+++ Junior-Programmer-Starter-Files/Assets/Scripts/ProductivityUnit.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class ProductivityUnit : Unit
 {
-
+    ResourcePile m_currentPile = null;
+    public float productivityMultiplier = 2.0f;
     protected override void BuildingInRange()
     {
-        
+        if (m_currentPile == null)
+        {
+            ResourcePile pile = m_Target as ResourcePile;
+
+            if (pile != null)
+            {
+                m_currentPile = pile;
+                m_currentPile.ProductionSpeed *= productivityMultiplier;
+            }
+        }
     }
 }
